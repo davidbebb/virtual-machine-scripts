b8b922b27db3e4f48f0d206628fa36a4f843013a
David Bebb <davidbebb@gmail.com>
2016-01-08 23:16:00 +0000

    Added upgrade to 14.04 resize script

--- ubuntu14.04-resize.sh
+++ ubuntu14.04-resize.sh
@@ -1,6 +1,7 @@
 
 sudo apt-get remove libcheese23
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get install xserver-xorg-core
 sudo apt-get install virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
 sudo ubuntu-drivers autoinstall
