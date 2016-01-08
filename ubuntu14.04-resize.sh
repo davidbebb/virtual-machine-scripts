f78a6ac94507cac1a923a2fb2d0e30855a3242d0
David Bebb <davidbebb@gmail.com>
2016-01-08 21:52:54 +0000

    Added additional steps to buntu 14.04 resize

--- ubuntu14.04-resize.sh
+++ ubuntu14.04-resize.sh
@@ -1,3 +1,7 @@
 
+sudo apt-get remove libcheese23
+sudo apt-get update
+sudo apt-get install xserver-xorg-core
 sudo apt-get install virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms
+sudo ubuntu-drivers autoinstall
 echo 'please restart the virtual machine'
