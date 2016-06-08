b8bb8e539b034087cd2901ea639608db329eae70
davidbebb <davidbebb@gmail.com>
2016-06-08 13:35:48 +0100

    added atom packages installer

---
+++ atom-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apm install linter-eslint
+apm install terminal-plus
+apm install merge-conflicts
+apm install atom-beautify
+apm install css-specificity
+apm install git-diff-details
+apm install git-time-machine
+apm install linter-js-yaml
+apm install linter-jsonlint
+apm install markdown-preview
+apm install merge-conflicts
