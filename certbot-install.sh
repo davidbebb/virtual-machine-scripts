a48fd1a830f4ad6c8dcce46d2dec9123eef2ced4
David Bebb <davidbebb@gmail.com>
2017-08-17 20:27:27 +0100

    certbot and jenkins

---
+++ certbot-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# shamlessly ripped from https://certbot.eff.org/#ubuntuxenial-other
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot
---
+++ pyenv.sh
@@ -0,0 +1,10 @@
+
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
+
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
