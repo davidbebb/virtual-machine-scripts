0822eb083f7092f8f043e7a6b7d7b2b0e2635ce0
David Bebb <davidbebb@gmail.com>
2020-10-11 18:15:55 +0100

    Switch to official package repo https://packagecloud.io/AtomEditor/atom

--- atom.sh
+++ atom.sh
@@ -1,3 +1,4 @@
-sudo add-apt-repository ppa:webupd8team/atom
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" | sudo tee /etc/apt/sources.list.d/atom.list
 sudo apt-get update
 sudo apt-get install atom
